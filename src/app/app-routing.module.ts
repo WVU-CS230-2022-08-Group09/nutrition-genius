d5d4afdfe306a926caec687ea64d98a80b31287e
TheBigOcean <jlo0015@mix.wvu.edu>
2022-10-13 08:06:29 -0400

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import {NavBarComponent} from './home-page/nav-bar/nav-bar.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path : '', component:HomePageComponent},
+  /* {path : 'Profile', component:}, */ 
+    
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
