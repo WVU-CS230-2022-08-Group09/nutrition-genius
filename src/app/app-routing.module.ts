5e5ea961fddad217f4e1fcc8f324cc006f85a2a8
Makayla Davis <105727646+med00005@users.noreply.github.com>
2022-10-17 18:32:36 -0400

    updated routing profile/homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
-import {NavBarComponent} from './home-page/nav-bar/nav-bar.component';
+import { ProfilePageComponent } from './profile-page/profile-page/profile-page.component';
 
 
 
