c0b7d9fc326e50fed679ae44fd36bb1e8f3e9b98
Makayla Davis <105727646+med00005@users.noreply.github.com>
2022-10-17 11:41:45 -0400

    updates to homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './home-page/nav-bar/nav-bar.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
