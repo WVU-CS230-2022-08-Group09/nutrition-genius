19d7c53dca9d9f7f64583faff3ffed4d49550ba2
smlough <111935529+smlough@users.noreply.github.com>
2022-11-10 06:52:38 -0500

    Add profile page
    Add profile page back

--- src/app/profile-page/profile-page/profile-page.component.spec.ts
+++ src/app/profile-page/profile-page/profile-page.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RecipeComponent } from './recipe.component';
+import { ProfilePageComponent } from './profile-page.component';
 
-describe('RecipeComponent', () => {
-  let component: RecipeComponent;
-  let fixture: ComponentFixture<RecipeComponent>;
+describe('ProfilePageComponent', () => {
+  let component: ProfilePageComponent;
+  let fixture: ComponentFixture<ProfilePageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RecipeComponent ]
+      declarations: [ ProfilePageComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(RecipeComponent);
+    fixture = TestBed.createComponent(ProfilePageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
