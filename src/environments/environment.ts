b9156c6e505b9906b42ff791d42d7aeb35f90299
colbygllhr <cjg0033@mix.wvu.edu>
2022-11-13 10:40:22 -0500

    added sdk's to environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,25 @@
 export const environment = {
   production: false
 };
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD-2f_Tk0S8vGm4xeZB3IQ5ptb740Tdfz4",
+  authDomain: "nutrition-genius.firebaseapp.com",
+  databaseURL: "https://nutrition-genius-default-rtdb.firebaseio.com",
+  projectId: "nutrition-genius",
+  storageBucket: "nutrition-genius.appspot.com",
+  messagingSenderId: "635447365282",
+  appId: "1:635447365282:web:479bc667528ebb7f3e51fb",
+  measurementId: "G-76TG9DZ5FM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
