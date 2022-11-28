90f96cb6e82f01d2afba827723878d6306cbe685
colbygllhr <cjg0033@mix.wvu.edu>
2022-11-28 16:10:47 -0500

    updated ingredient model

--- src/app/models/ingredient.model.ts
+++ src/app/models/ingredient.model.ts
@@ -1,3 +1,18 @@
+import { FactoryTarget } from "@angular/compiler";
+
 export class IngredientModel{
+    calories: number;
+    carbs: number;
+    fat: number;
+    protein: number;
+
+    constructor(calories: number, carbs: number, fat: number, protein: number){
+        this.calories = calories;
+        this.carbs = carbs;
+        this.fat = fat;
+        this.protein = protein;
+    }
     
+
+
 }
