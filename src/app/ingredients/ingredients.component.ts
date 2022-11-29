0a818890d5e04f86a016624d49bbe24eabbde914
colbygllhr <cjg0033@mix.wvu.edu>
2022-11-28 21:15:30 -0500

    updated ingredients.component.ts

--- src/app/ingredients/ingredients.component.ts
+++ src/app/ingredients/ingredients.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IngredientModel } from '../models/ingredient.model';
+import { IngredientsService } from './ingredients.service';
 
 @Component({
   selector: 'app-ingredients',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IngredientsComponent implements OnInit {
 
-  constructor() { }
+  ingredients: IngredientModel[] = [];
+
+
+
+  constructor(private ingredientsService: IngredientsService) {
+
+  }
+
+
 
   ngOnInit(): void {
+    this.ingredientsService.getIngredients().subscribe((data: IngredientModel[]) => {
+      for (var ingredient of data) {
+        this.ingredients.push(ingredient);
+      }
+    });
   }
 
 }
