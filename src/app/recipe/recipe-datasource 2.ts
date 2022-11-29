319efbf055c8e38f034ae503a56e15c3466d19e4
colbygllhr <cjg0033@mix.wvu.edu>
2022-11-28 19:44:22 -0500

    added service component for ingredients, updated .gitignore

---
+++ src/app/recipe/recipe-datasource 2.ts
@@ -0,0 +1,68 @@
+export let sampleData: Object[] = [
+    {
+        name: "Pancakes, Eggs & Bacon",
+        meal_type: "breakfast",
+        servings: 3,
+        meal: "Traditional American Breakfast",
+        calories: 460,
+        image: "assets/images/traditionalAmericanBreakfast.jpg"
+    },
+    {
+        name: "Buffalo Chicken Caesar Wraps",
+        meal_type: "lunch",
+        servings: 4,
+        meal: "",
+        calories: 520,
+        image: "assets/images/buffalo-chicken-wraps.jpg"
+
+    },
+    {
+        name: "Healthy Sriracha Shredded Chicken Tacos",
+        meal_type: "dinner",
+        servings: 4,
+        meal: "",
+        calories: 430,
+        image: "assets/images/tacos.jpg"
+    },
+    {
+        name: "Peanut Butter Oatmeal Bars",
+        meal_type: "dessert",
+        servings: 4,
+        meal: "",
+        calories: 280,
+        image: "assets/images/peanut-butter.jpg"
+    },
+    {
+        name: "Apple Cinnamon Crunch Overnight Oats",
+        meal_type: "breakfast",
+        servings: 4,
+        meal: "",
+        calories: 310,
+        image: "assets/images/oats.jpg"
+    },
+    {
+        name: "Barbecued chicken rice bowls",
+        meal_type: "lunch",
+        servings: 4,
+        meal: "",
+        calories: 390,
+        image: "assets/images/chicken-rice-bowl.jpg"
+    },
+    {
+        name: "Spaghetti & Spinach with Sun-Dried Tomato Cream Sauce",
+        meal_type: "dinner",
+        servings: 4,
+        meal: "",
+        calories: 480,
+        image: "assets/images/spaghetti.jpeg"
+    },
+    {
+        name: "Healthy Fruit Pizza with Oatmeal Cookie Crust",
+        meal_type: "dessert",
+        servings: 4,
+        meal: "",
+        calories: 320,
+        image: "assets/images/fruit-pizza.jpg"
+    }
+
+]
