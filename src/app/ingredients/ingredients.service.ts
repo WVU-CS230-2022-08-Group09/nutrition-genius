99979eb8c6e53864c3345b18ea42c4a731a56108
smlough <111935529+smlough@users.noreply.github.com>
2022-11-29 20:59:02 -0500

    ingredient updates for database service

--- src/app/ingredients/ingredients.service.ts
+++ src/app/ingredients/ingredients.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { IngredientModel } from "../models/ingredient.model";
+import { AngularFireDatabase } from "@angular/fire/compat/database";
 
 
 @Injectable(
@@ -11,22 +11,24 @@ export class IngredientsService {
     private baseUrl:string = "https://nutrition-genius-default-rtdb.firebaseio.com/";
     private ingredientsEndPoint = "ingredients.json";
 
-    constructor(private http:HttpClient){
+    // inject database into the component
+    constructor(private db:AngularFireDatabase) {
 
     }
-
-
-
 
     getIngredients() {
-        return this.http.get<IngredientModel []>(this.baseUrl + this.ingredientsEndPoint)
-    }
-    getIngredient(name:string){
-        return this.http.get<IngredientModel>(this.baseUrl + 'ingredients' + '/' + name + '.json');
+        // return the list of ingredients from the database
+        return this.db.list<IngredientModel>("ingredients").valueChanges();
     }
 
+    // not used yet
+    // getIngredient(name:string){
+    //     return this.http.get<IngredientModel>(this.baseUrl + 'ingredients' + '/' + name + '.json');
+    // }
+
     addIngredient(ingredient: IngredientModel) {
-        
+        // push new ingredient to the database
+        this.db.list<IngredientModel>("ingredient").push(ingredient);
     }
 
 } 
