ffe97125c15c623cb46fc3fa6219108d7383abb7
colbygllhr <cjg0033@mix.wvu.edu>
2022-12-11 10:46:48 -0500

    tried to fix firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
-export const environment = {
-  production: true
-};
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -9,18 +7,21 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD-2f_Tk0S8vGm4xeZB3IQ5ptb740Tdfz4",
-  authDomain: "nutrition-genius.firebaseapp.com",
-  databaseURL: "https://nutrition-genius-default-rtdb.firebaseio.com",
-  projectId: "nutrition-genius",
-  storageBucket: "nutrition-genius.appspot.com",
-  messagingSenderId: "635447365282",
-  appId: "1:635447365282:web:479bc667528ebb7f3e51fb",
-  measurementId: "G-76TG9DZ5FM"
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD-2f_Tk0S8vGm4xeZB3IQ5ptb740Tdfz4",
+    authDomain: "nutrition-genius.firebaseapp.com",
+    databaseURL: "https://nutrition-genius-default-rtdb.firebaseio.com",
+    projectId: "nutrition-genius",
+    storageBucket: "nutrition-genius.appspot.com",
+    messagingSenderId: "635447365282",
+    appId: "1:635447365282:web:479bc667528ebb7f3e51fb",
+    measurementId: "G-76TG9DZ5FM"
+  }
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(environment.firebase);
 const analytics = getAnalytics(app);
 
